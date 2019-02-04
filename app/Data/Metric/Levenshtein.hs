37cbd83179067082bae21260573a96d035a7f405
Alexander Ben Nasrallah <me@abn.sh>
2019-02-04 20:04:45 +0100

    use State monad for cached computation

--- app/Data/Metric/Levenshtein.hs
+++ app/Data/Metric/Levenshtein.hs
@@ -2,36 +2,39 @@ module Data.Metric.Levenshtein
   ( levenshtein
   ) where
 
-import qualified Data.Map.Strict as Map
+import           Control.Monad.State
+import qualified Data.Map.Strict     as Map
 
 levenshtein :: (Eq a) => [a] -> [a] -> Int
 levenshtein s1 s2 = d
   where
-    (d, _) = _levenshtein Map.empty s1 s2 0 0
+    (d, _) = runState (_levenshtein s1 s2 0 0) Map.empty
 
-_levenshtein :: (Eq a) => Cache -> [a] -> [a] -> Int -> Int -> (Int, Cache)
-_levenshtein cache [] y idx idy =
-  (length y, Map.insert (idx, idy) (length y) cache)
-_levenshtein cache x [] idx idy =
-  (length x, Map.insert (idx, idy) (length x) cache)
-_levenshtein cache s@(x:xs) t@(y:ys) idx idy = (m, Map.insert (idx, idy) m c3)
-  where
-    m = minimum [a + 1, b + 1, c + fromEnum (x /= y)]
-    (a, c1) = fromCache cache _levenshtein xs t (idx + 1) idy
-    (b, c2) = fromCache c1 _levenshtein s ys idx (idy + 1)
-    (c, c3) = fromCache c2 _levenshtein xs ys (idx + 1) (idy + 1)
+_levenshtein :: (Eq a) => [a] -> [a] -> Int -> Int -> State Cache Int
+_levenshtein [] y idx idy = insert (idx, idy) (length y) >> return (length y)
+_levenshtein x [] idx idy = insert (idx, idy) (length x) >> return (length x)
+_levenshtein s@(x:xs) t@(y:ys) idx idy = do
+  a <- fromCache _levenshtein xs t (idx + 1) idy
+  b <- fromCache _levenshtein s ys idx (idy + 1)
+  c <- fromCache _levenshtein xs ys (idx + 1) (idy + 1)
+  let m = minimum [a + 1, b + 1, c + fromEnum (x /= y)]
+  insert (idx, idy) m
+  return m
 
 type Cache = Map.Map (Int, Int) Int
 
+insert :: (Int, Int) -> Int -> State Cache ()
+insert k v = state $ \cache -> ((), Map.insert k v cache)
+
 fromCache ::
-     Cache
-  -> (Cache -> a -> b -> Int -> Int -> (Int, Cache))
+     (a -> b -> Int -> Int -> State Cache Int)
   -> a
   -> b
   -> Int
   -> Int
-  -> (Int, Cache)
-fromCache cache f x y n m =
-  case Map.lookup (n, m) cache of
-    Nothing -> f cache x y n m
-    Just j  -> (j, cache)
+  -> State Cache Int
+fromCache f x y n m =
+  state $ \cache ->
+    case Map.lookup (n, m) cache of
+      Nothing -> runState (f x y n m) cache
+      Just j  -> (j, cache)
